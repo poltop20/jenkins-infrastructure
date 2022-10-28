3a5c0bfa6b102a0eb38f5793a5d68f9a6fe4d30a
poltop20 <poltop20@yahoo.com>
2022-10-27 21:38:25 -0400

    adding webserver infra

---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "ami" {
+    description = "ami of instance"
+    default = "ami-017c001a88dd93847"
+}
+
+variable "instance_type" {
+    description = "Type of instance"
+    default =  "t2.micro"
+}
