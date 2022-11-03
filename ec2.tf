673851468d545ad45e29dfdba96e4cc2034f9b8a
poltop20 <poltop20@yahoo.com>
2022-11-03 19:41:02 -0400

    modified chekov

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "ec2" {
     ami = var.ami
     instance_type = var.instance_type
+    monitoring = true
+    ebs_optimized = true
     tags = {
     Name = "jenkins-tf-webserver"
     }
