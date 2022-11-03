d7fa35f98a40beb4c36c55462aa3f27da12cfa45
poltop20 <poltop20@yahoo.com>
2022-11-03 19:57:23 -0400

    ebs encrypted

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "ec2" {
     instance_type = var.instance_type
     monitoring = true
     ebs_optimized = true
+    root_block_device {
+    encrypted     = true
+    }
     tags = {
     Name = "jenkins-tf-webserver"
     }
