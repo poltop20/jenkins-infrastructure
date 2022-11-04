7469a2b0a62f2a3c8ab1fa8dc028f7a153c118ac
poltop20 <poltop20@yahoo.com>
2022-11-03 21:11:03 -0400

    modified

--- s3.tf
+++ s3.tf
@@ -1,5 +1,14 @@
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
+  block_public_acls   = true
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+       sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
   tags = {
     Name        = "My bucket"
